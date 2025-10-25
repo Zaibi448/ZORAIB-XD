90fb57e31ae9f2f649393539e0926e4d6f2d62ec
Zaibi448 <ukzoraib@gmail.com>
2025-10-25 14:52:16 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -13,7 +13,7 @@ cmd({
     filename: __filename,
 },
 async (conn, mek, m, { from, reply }) => {
-    const githubRepoURL = 'https://github.com/Zaibi448/ZORAIB-XD';
+    const githubRepoURL = 'https://github.com/Arslan-MD/Arslan_MD';
 
     try {
         // Extract username and repo name from the URL
@@ -29,7 +29,7 @@ async (conn, mek, m, { from, reply }) => {
         const repoData = await response.json();
 
         // Format the repository information
-        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *`ZORAIBXHASSAN`🖤*`;
+        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟🖤*`;
 
         // Send image with caption
         await conn.sendMessage(from, {
@@ -40,7 +40,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363405633935764@newsletter',
+                    newsletterJid: '120363401862633795@newsletter',
                     newsletterName: 'ZORAIB-XD',
                     serverMessageId: 143
                 }
@@ -58,7 +58,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363405633935764@newsletter',
+                    newsletterJid: '120363401862633795@newsletter',
                     newsletterName: 'ZORAIB-XD',
                     serverMessageId: 143
                 }

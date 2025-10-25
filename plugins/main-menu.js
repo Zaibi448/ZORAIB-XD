3b37a65d6464f8fd3145747714ee3fc543c9e3da
Zaibi448 <ukzoraib@gmail.com>
2025-10-25 13:52:10 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -346,14 +346,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/yrpxq6.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://bumpy-coral-egnhazoogd.edgeone.app/20251024_200812.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363401862633795@newsletter',
+                        newsletterJid: '120363405633935764@newsletter',
                         newsletterName: config.BOT_NAME,
                         serverMessageId: 143
                     }

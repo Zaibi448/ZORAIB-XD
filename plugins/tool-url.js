28a1282d351aaed24a8b23a36f3fa65182992723
Zaibi448 <ukzoraib@gmail.com>
2025-11-02 14:00:54 +0500

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `>  𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟 💜`
+      `>  `Zoraib` 💜`
     );
 
   } catch (error) {
